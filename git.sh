2daac1d3a6a871d49834efab111169692608cde6
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-09-28 17:34:55 -0300

    documentation for the git cloner function

--- git.sh
+++ git.sh
@@ -4,6 +4,23 @@ import path
 local_git_github_user_repos=https://api.github.com/users/*/repos
 
 function git_clone_repos_user() {
+    : '
+    Given a user name clone all the repositories
+    from the github account
+    for example the user ingadhoc has this repositories
+
+        ./account-analytic/
+        ./account-financial-tools/
+        ./account-invoicing/
+        ./account-payment/
+        ./adhoc-rancher/
+        ./aeroo/
+
+    so:
+    git_clone_repos_user ingadhoc
+
+    will create a copy for each one in a folder with that username
+    '
     IFS=
     # create folder for user
     if [ ! -d "$1" ]
