5178bcb2faae41d12491665e2c0dd4b8a22f29fd
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-09-28 16:36:29 -0300

    json.sh library

--- base.sh
+++ base.sh
@@ -1,26 +1,8 @@
 . ~/mromay_bashrc/imports.sh
-import char
 import logging
+import char
 import odoo
 import docker
-
-function post_json() {
-	json=$(to_json "${@:2}")
-	if [ $(char_count $json) -gt 4 ]
-	then
-		curl -H "Content-Type: application/json" -X POST -d "$json" $1
-	else
-		echo NOT ENOUGH DATA
-	fi
-}
-
-function to_json() {
-	IFS=
-	lines=$(echo $@ | egrep -o "[^ ]+ [^ ]+")
-	lines=$(echo $lines | sed 's/ /": "/g')
-	lines=$(echo $lines | tr -s '\n' '/' | sed 's/.$//g')
-	lines=$(echo $lines | sed 's/\//", "/g')
-	echo '{"'$lines'"}'
-}
+import json
 
 loaded base
