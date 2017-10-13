0a1c7644774d5b0552725b4aff6a55486d58398c
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-10-13 11:45:19 -0300

    improved function documentation

--- python.sh
+++ python.sh
@@ -2,9 +2,19 @@
 import logging
 
 function python_package_path() {
+    : '
+    IF YOU WANT TO KNOW THE PATH OF A PYTHON PACKAGE
+    USE THIS FUNCTION IN THE FOLLOWING MANNER
+
+    python_package_path json
+
+    WILL RETURN
+
+    /usr/lib/python2.7/json
+    '
     if [ $1 ]
     then
-	python2 -c "import "$1"; print "$1".__file__"
+	dirname $(python2 -c "import "$1"; print "$1".__file__") 2> /dev/null
     fi
 }
 
