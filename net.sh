f128d220135e014740f90677992f7f028c5ff980
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-10-14 18:01:01 -0300

    imprived net.sh and more documentation

--- net.sh
+++ net.sh
@@ -1,11 +1,33 @@
 . $home/mromay_bashrc/imports.sh
 import logging
 
-function myip () {
-    dig +short myip.opendns.com @resolver1.opendns.com
-    # curl ipecho.net/plain
-    # curl icanhazip.com
-    # curl ifconfig.me
+function net_myip () {
+    : '
+    TRIES TO GET THE WLAN IP OF
+    THIS MACHINE FROM MULTIPLE SOURCES
+    '
+    p=$(dig +short myip.opendns.com @resolver1.opendns.com | grepip)
+    if [ ! $p ]
+    then
+	p=$(curl ipecho.net/plain | grepip)
+    fi
+    if [ ! $p ]
+    then
+	p=$(curl icanhazip.com | grepip)
+    fi
+    if [ ! $p ]
+    then
+	p=$(curl ifconfig.me | grepip)
+    fi
+    echo $p
+}
+
+
+function net_check() {
+    : '
+    PINGS TO 8.8.8.8 GOOGLE SERVER TO TEST WLAN
+    '
+    ping 8.8.8.8
 }
 
 loaded net
