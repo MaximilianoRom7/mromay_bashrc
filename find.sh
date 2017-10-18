0e4c7930bb716d37e856061dec290a96a5493f4f
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-10-18 12:31:30 -0300

    fix if else login in findd function again

--- find.sh
+++ find.sh
@@ -9,17 +9,17 @@ function findd() {
     $2 IS THE PATH
     $3 IS THE DEPTH
     '
-    if [[ $1 && $2 && $3 ]]
+    if [ "$3" ]
     then
-	find $2 -maxdepth $3 -type d -name \*$1\*
+	find "$2" -maxdepth "$3" -type d -name \*"$1"\*
     else
-	if [[ $1 && $2 ]]
+	if [ "$2" ]
 	then
-	    find $2 -type d -name \*$1\*
+	    find "$2" -type d -name \*"$1"\*
 	else
-	    if [[ $1 ]]
+	    if [ "$1" ]
 	    then
-		find . -type d -name \*$1\*
+		find . -type d -name \*"$1"\*
 	    else
 		find . -type d
 	    fi
