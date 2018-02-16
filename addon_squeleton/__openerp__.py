78c9f4ee6ccfe33ae76b18d3fc2264febce53445
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2018-02-16 14:22:20 -0300

    new folder addon_squeleton with model.py and view.xml

---
+++ addon_squeleton/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': '',
+    'description': '',
+    'author': 'Fusion S.A',
+    'depends': [
+        
+    ],
+    # todo lo que esta en esta lista data
+    # son paths de archivos .xml .csv
+    # 'views/...'
+    'data': [
+        
+    ]
+    'application': True,
+}
---
+++ addon_squeleton/model.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models, _
+
+class MiClase(models.model):
+    """
+    No hace nada
+    """
+    # _name = 'nombre de la tabla'
+    
+    # campos
+    # campo1 = fields.Char("campo 1")
+    pass
