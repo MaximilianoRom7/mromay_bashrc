1e4e3a9aa07c20044a714fdbcfecd4e949221509
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2019-02-28 00:45:49 -0300

    vars.sh file

---
+++ vars.sh
@@ -0,0 +1,9 @@
+global_logging=
+prompt_function=1
+repos=/home/$USER/odoo/repos
+
+# disable ssl in git clone
+GIT_SSL_NO_VERIFY=true
+
+# access control disabled, clients can connect from any host
+xhost +
