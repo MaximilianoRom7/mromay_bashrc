f08c28c6a56eccb661f62544de05cc3dbce8bc26
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2019-02-28 00:39:49 -0300

    xonsh scripts

--- vars.sh
+++
@@ -1,6 +0,0 @@
-global_logging=1
-prompt_function=1
-repos=/home/$USER/odoo/repos
-
-# access control disabled, clients can connect from any host
-xhost +
--- xonsh_cripts/xonshrc.py
+++ xonsh_cripts/xonshrc.py
@@ -1,23 +1,21 @@
 """
 THIS PYTHON SCRIPT IS MEANT TO BE EXECUTED USING XONSH
-
 XONSH IS A BASH LIKE INTERPRETER WRITED IN PYTHON
-
 INSTALL IT USING
-
 SUDO PIP INSTALL XONSH
+THEN CALL IT WITH
+XONSH
+AND ALL THE FUNCTION IN THIS PYTHON SCRIPT
+CAN BE USED JUST LIKE A NORMAL BASH FUNCTION
 """
 import socket
+import sys
+
+sys.path = ["/root/mromay_bashrc/xonsh_cripts"] + sys.path
+
+from local import *
 
 def _domain_ip(args):
     return socket.gethostbyname(args[0])
-
-def _newline(func):
-    """
-    CALLS THE ORIGINAL FUNCTION APPENDING A NEW LINE AT THE END
-    """
-    def inner(args):
-        return str(func(args)) + "\n"
-    return inner
 
 aliases['domain_ip'] = _newline(_domain_ip)
